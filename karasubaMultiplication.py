75a93b722aaa76716750a0db969089e3f3b53a0f
RuoChen95 <xrc_1995@163.com>
2019-01-20 21:09:02 +0800

    minor update

--- karasubaMultiplication.py
+++ karasubaMultiplication.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-# alfa = 5678
+# alpha = 5678
 # beta = 1234
 
-alfa = 3141592653589793238462643383279502884197169399375105820974944592
+alpha = 3141592653589793238462643383279502884197169399375105820974944592
 beta = 2718281828459045235360287471352662497757247093699959574966967627
+
 
 def divide(x, y):
 
@@ -19,15 +20,15 @@ def divide(x, y):
         # 字符串无法做乘法
         return int(x) * int(y)
 
-    xLength = int(len(str(x)) / 2)
-    yLength = int(len(str(y)) / 2)
+    x_length = int(len(str(x)) / 2)
+    y_length = int(len(str(y)) / 2)
 
-    a = str(x)[0:xLength]
-    b = str(x)[xLength:]
-    c = str(y)[0:yLength]
-    d = str(y)[yLength:]
+    a = str(x)[0:x_length]
+    b = str(x)[x_length:]
+    c = str(y)[0:y_length]
+    d = str(y)[y_length:]
 
-    # print xLength, yLength
+    # print x_length, y_length
     # print a, b, c, d
     # return false
 
@@ -40,4 +41,4 @@ def merge(a, b, c, d):
     return 10 ** n * divide(a, c) + 10 ** (n/2) * (divide(a, d) + divide(b, c)) + divide(b, d)
 
 
-print divide(alfa, beta)
+print divide(alpha, beta)

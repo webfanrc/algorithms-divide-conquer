03802bd7e0ba43b5bc465af20aca390699852ef2
RuoChen95 <xrc_1995@163.com>
2019-02-01 12:00:42 +0800

    quickSort: pass first and middle

--- quickSort.js
+++ quickSort.js
@@ -10,7 +10,7 @@ for (let i = 0; i < array.length; i ++) {
 
 let m = 0;
 
-//array = [5,4,3,2,1];
+array = [2, 20, 1, 15, 3, 11, 13, 6, 16, 10, 19, 5, 4, 9, 8, 14, 18, 17, 7, 12];
 
 console.log('input:', array.length)
 
@@ -35,38 +35,55 @@ function partition(A, l, r) {
     // A.splice(l,0,array_cache);
     // let p = A[l];
 
+    // let array_cache = A[r-1];
+    // A[r-1] = A[l];
+    // A[l] = array_cache;
+    // let p = A[l];
+
 
     // middle of it
+    // Identify which of these three elements is the median (i.e., the one whose value is in between the other two)
+    // tip: https://www.coursera.org/learn/algorithms-divide-conquer/discussions/weeks/3/threads/3svB7I86Eee8LxLjS85WxA
+    // tip: https://www.coursera.org/learn/algorithms-divide-conquer/discussions/weeks/3/threads/fG_c7gxREeiFUgpNZCwT4A
     let p = 0;
-    let new_array = [A[l],A[parseInt((l+r-1)/2)],A[r-1]];
-    //console.log(new_array);
-    let new_array_sort = new_array.sort();
-    //console.log(new_array_sort)
-    let middleNumber = new_array_sort[1];
-    for (let m = 0; m < new_array.length; m++) {
-        if (new_array[m] == middleNumber) {
-            if (m == 0) {
-                p = A[l]
-            }
-            if (m == 1) {
-                let array_cache = A[parseInt((l+r-1)/2)];
-                A.splice(parseInt((l+r-1)/2),1);
-                A.splice(l,0,array_cache);
-
-                p = A[l];
-            }
-            if (m == 2) {
-                // last of it
-                let array_cache = A[r-1];
-                A.splice(r-1,1);
-                A.splice(l,0,array_cache);
-
-                p = A[l];
-            }
-        }
+    let k = 0;
+    if ((r - l) % 2 == 0) {
+        k = l + parseInt((r-l)/2);
+    } else {
+        k = l + parseInt((r-l)/2);
     }
 
+    let new_array = [A[l],A[k],A[r-1]];
+    let new_array_sort = [A[l],A[k],A[r-1]].sort();
+    let middle_number = new_array_sort[1];
 
+    for (let m = 0; m < new_array.length; m++) {
+        if (new_array[m] == middle_number) {
+            if (m == 1) {
+                // let array_cache = A[parseInt((l+r-1)/2)];
+                // A.splice(parseInt((l+r-1)/2),1);
+                // A.splice(l,0,array_cache);
+                // p = A[l];
+
+                // another way
+                let array_cache = A[k];
+                A[k] = A[l];
+                A[l] = array_cache;
+            } else if (m == 2) {
+                // last of it
+                // let array_cache = A[r-1];
+                // A.splice(r-1,1);
+                // A.splice(l,0,array_cache);
+                // p = A[l];
+
+                // another way:
+                let array_cache = A[r-1];
+                A[r-1] = A[l];
+                A[l] = array_cache;
+            }
+            p = A[l];
+        }
+    }
 
     let i = l + 1;
 

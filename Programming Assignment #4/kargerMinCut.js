97c52483fc9fed99c3dd35a2b604caeba4111519
RuoChen95 <xrc_1995@163.com>
2019-02-09 16:40:00 +0800

    update of pa#4(mid term)

--- Programming Assignment #4/kargerMinCut.js
+++ Programming Assignment #4/kargerMinCut.js
@@ -1,15 +1,14 @@
 let rs = require("fs");
 
-let data = rs.readFileSync("test.txt","utf-8");
+let data = rs.readFileSync("kargerMinCut.txt","utf-8");
 
 let array = data.split('\n');
 
 let vertices = [];
 let edges = [];
 
-
 for (let i = 0; i < array.length; i ++) {
-  array[i] = array[i].split(' ').map(function(n, index) {
+  array[i] = array[i].split('\t').map(function(n, index) {
     return parseInt(n)
   })
 }
@@ -19,7 +18,6 @@ vertices = array.map(function(n) {
 })
 
 array.forEach(function (n) {
-  console.log(n)
   n.forEach(function (m, index, array) {
     // 数组是引用类型
     if (index >= 1 && !edges.some(function (arr) {return JSON.stringify(arr) === JSON.stringify([m, array[0]])})) {
@@ -28,9 +26,9 @@ array.forEach(function (n) {
   })
 })
 
-console.log(vertices)
-console.log(edges)
-console.log(array)
+// console.log(vertices)
+// console.log(edges)
+// console.log(array)
 
 function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
@@ -48,22 +46,31 @@ While there are more than 2 vertices:
 
 // 图的合并
 function contract(vertices, edges) {
+  // console.log('vertices',vertices)
+  // console.log('edges',edges)
   if (vertices.length > 2) {
     let ind = getRandomIntInclusive(0, edges.length-1);
 
-    let pickedEdges = edges[ind];
+    let u = edges[ind][0];
+    let v = edges[ind][1];
+
+    vertices.splice(vertices.indexOf(v),1);
+
+    let newList = [];
+
     edges.splice(ind,1);
 
-    // 合并
-    // ？
+    for (let i = 0; i < edges.length; i++) {
+      if (edges[i][0] == v) edges[i][0] = u;
+      if (edges[i][1] == v) edges[i][1] = u;
+      if (edges[i][0] !== edges[i][1]) newList.push(edges[i])
+    }
 
-
-    contract(vertices, edges)
+    return contract(vertices, newList)
   } else {
-    // 计算
-    // ?
+    return edges.length
   }
 }
 
+// 多次计算获取最小值
 console.log(contract(vertices, edges))
-

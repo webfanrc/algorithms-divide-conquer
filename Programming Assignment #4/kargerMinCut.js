fedb126fd160a89eed226b85685398863d7dfb9f
RuoChen95 <xrc_1995@163.com>
2019-02-10 15:55:46 +0800

    增加多次计算

--- Programming Assignment #4/kargerMinCut.js
+++ Programming Assignment #4/kargerMinCut.js
@@ -1,79 +1,98 @@
-let rs = require("fs");
+function main() {
 
-let data = rs.readFileSync("kargerMinCut.txt","utf-8");
-// let data = rs.readFileSync("test.txt","utf-8");
+  let rs = require("fs");
 
+  let data = rs.readFileSync("kargerMinCut.txt","utf-8");
+  // let data = rs.readFileSync("test.txt", "utf-8");
 
-let array = data.split('\n');
+  let array = data.split('\n');
 
-let inputVertices = [];
-let inputEdges = [];
+  let inputVertices = [];
+  let inputEdges = [];
 
-for (let i = 0; i < array.length; i ++) {
-  array[i] = array[i].split(' ').map(function(n, index) {
-    return parseInt(n)
+  // console.log(array)
+
+  for (let i = 0; i < array.length; i++) {
+    array[i] = array[i].split('\t').map(function (n, index) {
+      return parseInt(n)
+    })
+  }
+
+  inputVertices = array.map(function (n) {
+    return n[0]
   })
-}
 
-inputVertices = array.map(function(n) {
-  return n[0]
-})
-
-array.forEach(function (n) {
-  n.forEach(function (m, index, array) {
-    // 数组是引用类型
-    if (index >= 1 && !inputEdges.some(function (arr) {return JSON.stringify(arr) === JSON.stringify([m, array[0]])})) {
-      inputEdges.push([array[0], m])
-    }
+  array.forEach(function (n) {
+    n.forEach(function (m, index, array) {
+      // 数组是引用类型
+      if (index >= 1 && !inputEdges.some(function (arr) {
+        return JSON.stringify(arr) === JSON.stringify([m, array[0]])
+      })) {
+        inputEdges.push([array[0], m])
+      }
+    })
   })
-})
 
 // console.log(inputVertices)
 // console.log(inputEdges)
 // console.log(array)
 
-function getRandomIntInclusive(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
-}
+  function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+  }
 
-/*
-**** Karger's Min-Cut Algorithm ****
+  /*
+  **** Karger's Min-Cut Algorithm ****
 
-While there are more than 2 vertices:
-1. pick a remaining edge (u,v) uniformly at random
-2. merge (or "contract") u and v into a single vertex
-3. remove self-loops
-4. return cut represented by final 2 vertices.
-*/
+  While there are more than 2 vertices:
+  1. pick a remaining edge (u,v) uniformly at random
+  2. merge (or "contract") u and v into a single vertex
+  3. remove self-loops
+  4. return cut represented by final 2 vertices.
+  */
 
 // 图的合并
-function contract(vertices, edges) {
-  if (vertices.length > 2) {
-    let ind = getRandomIntInclusive(0, edges.length-1);
+  function contract(vertices, edges) {
+    // console.log(vertices)
+    // console.log(edges)
+    if (vertices.length > 2) {
+      let ind = getRandomIntInclusive(0, edges.length - 1);
 
-    let u = edges[ind][0];
-    let v = edges[ind][1];
+      let u = edges[ind][0];
+      let v = edges[ind][1];
 
-    vertices.splice(vertices.indexOf(v),1);
+      vertices.splice(vertices.indexOf(v), 1);
 
-    let newList = [];
+      let newList = [];
 
-    edges.splice(ind,1);
+      edges.splice(ind, 1);
 
-    for (let i = 0; i < edges.length; i++) {
-      if (edges[i][0] == v) edges[i][0] = u;
-      if (edges[i][1] == v) edges[i][1] = u;
-      if (edges[i][0] !== edges[i][1]) newList.push(edges[i])
+      for (let i = 0; i < edges.length; i++) {
+        if (edges[i][0] == v) edges[i][0] = u;
+        if (edges[i][1] == v) edges[i][1] = u;
+        if (edges[i][0] !== edges[i][1]) newList.push(edges[i])
+      }
+
+      return contract(vertices, newList)
+    } else {
+      return edges.length
     }
-
-    return contract(vertices, newList)
-  } else {
-    return edges.length
   }
+  // console.log('the number of vertices n', inputVertices.length)
+  // console.log('the lowest possibilities', 1/(inputVertices.length * inputVertices.length))
+  // console.log('the number of edges m', inputEdges.length)
+  return contract(inputVertices, inputEdges)
 }
 
+
+console.time("time");
 // 多次计算获取最小值
-// TODO 随机计算
-console.log(contract(inputVertices, inputEdges))
-console.log(contract(inputVertices, inputEdges)) //?
-console.log(contract(inputVertices, inputEdges)) //?
+let k = main(); // 4000ms
+for (let i = 0; i < 20; i ++) {
+  let kInterval = main();
+  if (k > kInterval) k = kInterval
+}
+
+console.log(k)
+
+console.timeEnd("time");
